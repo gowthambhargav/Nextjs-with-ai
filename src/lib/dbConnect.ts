e6461182bdc19b039187f22a3641bb36fa16f05c
gowthambhargav <gowthambhargav26@gmail.com>
2024-04-21 08:26:28 +0530

    Update dbConnect function to include database connection logging and error handling

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -12,8 +12,16 @@ async function dbConnect():Promise<void>{
         return
     }
     try {
-        
+      const db =   await mongoose.connect(process.env.MONGO_URI || "")
+      console.log(db);
+    connection.isConnexted =  db.connections[0].readyState
+      console.log("Db connected Successfully");
+      
     } catch (error) {
+        console.log("Database connection failed",error);
         
+        process.exit(1)
     }
 }
+
+export default dbConnect
